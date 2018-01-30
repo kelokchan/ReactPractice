5ee76b7bbdbfe4e666ff47449b93d8b0a1b280ab
Kelok Chan <kelokchan@gmail.com>
2018-01-30 16:31:07 +0800

    complete section 3

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,107 +1,38 @@
 'use strict';
 
-// JSX - Javascript XML
-var app = {
-  title: 'Indecision App',
-  subtitle: 'This is some info',
-  options: ['One', 'Two']
-};
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    'p',
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options.length > 0 ? 'Here are your options' : 'No options'
-  ),
-  React.createElement(
-    'ol',
-    null,
-    React.createElement(
-      'li',
-      null,
-      'Item one'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'Item two'
-    )
-  )
-);
+var visibility = false;
 
-// const user = {
-//   // name: 'Kelok',
-//   age: 22,
-//   location: 'Brisbane'
-// };
-// const getLocation = (location) => {
-//   if (location) {
-//     return <p> Location: {location} </p>;
-//   }
-// }
-// const templateTwo = (
-//   <div>
-//     <h1>{user.name ? user.name : 'Anonymous'}</h1>
-//     {user.age >= 18 && <p>Age: {user.age}</p>}  {/* Show second when both true */}
-//     {getLocation(user.location)}
-//   </div>
-// );
-
-var count = 0;
-var addOne = function addOne() {
-  count++;
-  renderCounterApp();
-};
-var minusOne = function minusOne() {
-  count--;
-  renderCounterApp();
-};
-var reset = function reset() {
-  count = 0;
-  renderCounterApp();
+var toggleVisibility = function toggleVisibility() {
+  visibility = !visibility;
+  render();
 };
 
-var appRoot = document.getElementById('app');
-
-var renderCounterApp = function renderCounterApp() {
-  var templateTwo = React.createElement(
+var render = function render() {
+  var jsx = React.createElement(
     'div',
     null,
     React.createElement(
       'h1',
       null,
-      'Count: ',
-      count
+      'Visibility Toggle'
     ),
     React.createElement(
       'button',
-      { onClick: addOne },
-      '+1'
+      { onClick: toggleVisibility },
+      visibility ? 'Hide details' : 'Show details'
     ),
-    React.createElement(
-      'button',
-      { onClick: minusOne },
-      '-1'
-    ),
-    React.createElement(
-      'button',
-      { onClick: reset },
-      'reset'
+    visibility && React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'h1',
+        null,
+        'Hey. These are toggled on'
+      )
     )
   );
 
-  ReactDOM.render(templateTwo, appRoot);
+  ReactDOM.render(jsx, document.getElementById('app'));
 };
 
-renderCounterApp();
+render();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,65 +2,54 @@
 const app = {
   title: 'Indecision App',
   subtitle: 'This is some info',
-  options: ['One', 'Two']
+  options: []
 };
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-  </div>
-);
 
-// const user = {
-//   // name: 'Kelok',
-//   age: 22,
-//   location: 'Brisbane'
-// };
-// const getLocation = (location) => {
-//   if (location) {
-//     return <p> Location: {location} </p>;
-//   }
-// }
-// const templateTwo = (
-//   <div>
-//     <h1>{user.name ? user.name : 'Anonymous'}</h1>
-//     {user.age >= 18 && <p>Age: {user.age}</p>}  {/* Show second when both true */}
-//     {getLocation(user.location)}
-//   </div>
-// );
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.option.value; // get the form option input value
 
-let count = 0;
-const addOne = () => {
-  count++;
-  renderCounterApp();
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
 };
-const minusOne = () => {
-  count--;
-  renderCounterApp();
+
+const onRemoveAll = () => {
+  app.options = [];
+  render();
 };
-const reset = () => {
-  count = 0;
-  renderCounterApp();
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
 };
 
 const appRoot = document.getElementById('app');
 
-const renderCounterApp = () => {
-  const templateTwo = (
+const render = () => {
+  const template = (
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>reset</button>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        {
+          app.options.map((option) => <li key={option}>{option}</li>)
+        }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
     </div>
   );
 
-  ReactDOM.render(templateTwo, appRoot);
-};
+  ReactDOM.render(template, appRoot);
+};  
 
-renderCounterApp();
+render();
